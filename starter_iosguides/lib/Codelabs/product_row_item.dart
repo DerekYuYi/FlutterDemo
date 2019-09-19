44180bd4a0e304c5297b62232877b14a18cf4dc6
Yuyi <1363364123@qq.com>
2019-09-19 22:40:01 +0800

    Adds search list for CupertinoStoreApp.

--- starter_iosguides/lib/Codelabs/product_row_item.dart
+++ starter_iosguides/lib/Codelabs/product_row_item.dart
@@ -28,55 +28,60 @@ class ProductRowItem extends StatelessWidget {
         bottom: 8,
         right: 8,
       ),
-      child: Row(
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(4),
-            child: Image.asset(
-              product.assetName,
-              package: product.assetPackage,
-              fit: BoxFit.cover,
-              width: 76,
-              height: 76,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Row(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Image.asset(
+                  product.assetName,
+                  package: product.assetPackage,
+                  fit: BoxFit.cover,
+                  width: 76,
+                  height: 76,
 //              color: Styles.scaffoldBackground,
-            ),
-          ),
-          
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    product.name,
-                    style: Styles.productRowItemName,
-                  ),
-                  const Padding(padding: EdgeInsets.only(top: 8)),
-                  Text(
-                    '\$${product.price}',
-                    style: Styles.productRowItemPrice,
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
 
-          CupertinoButton(
-            padding: EdgeInsets.zero,
-            child: const Icon(
-              CupertinoIcons.plus_circled,
-              semanticLabel: 'Add',
-            ),
-            onPressed: () {
-              // 直接管理状态？？
-              final model = Provider.of<AppStateModel>(context);
-              model.addProductToCart(product.id);
-            },
-          ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        product.name,
+                        style: Styles.productRowItemName,
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      Text(
+                        '\$${product.price}',
+                        style: Styles.productRowItemPrice,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
 
-        ],
+              CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: const Icon(
+                  CupertinoIcons.plus_circled,
+                  semanticLabel: 'Add',
+                ),
+                onPressed: () {
+                  // 直接管理状态？？
+                  final model = Provider.of<AppStateModel>(context);
+                  model.addProductToCart(product.id);
+                },
+              ),
+
+            ],
+          ),
+        ),
       ),
     );
 
--- starter_iosguides/lib/Codelabs/search_tab.dart
+++ starter_iosguides/lib/Codelabs/search_tab.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
-
+import 'package:provider/provider.dart';
+import 'model/app_state_model.dart';
+import 'product_row_item.dart';
+import 'styles.dart';
+import 'search_bar.dart';
 
 /// 搜索页面。是一个 stateful widget, 因为随着用户输入文本的变化，列表中的内容也会随之变化
 class SearchTab extends StatefulWidget {
@@ -10,16 +14,79 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
+
+  TextEditingController _controller;
+  FocusNode _focusNode;
+  String _terms = '';
+
+  @override
+  // 创建时调用一次
+  void initState() {
+    super.initState();
+    _controller = TextEditingController()..addListener(_onTextChanged);
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTextChanged() {
+    setState(() {
+      _terms = _controller.text;
+    });
+  }
+
+  Widget _buildSearchBox() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: SearchBar(
+        controller: _controller,
+        focusNode: _focusNode,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    // 通过嵌套 于CustomScrollView 中的 CupertinoSliverNavigationBar 来实现 iOS11 上的大标题
-    return CustomScrollView(
-      slivers: const <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Search'),
+    // 拿到全局 AppStateModel 类实例
+    final model = Provider.of<AppStateModel>(context);
+    final results = model.search(_terms);
+
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        color: Styles.scaffoldBackground,
+      ),
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            _buildSearchBox(),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) => ProductRowItem(
+                  index: index,
+                  product: results[index],
+                  lastItem: index == results.length - 1,
+                ),
+                itemCount: results.length,
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
+
+    // 通过嵌套 于CustomScrollView 中的 CupertinoSliverNavigationBar 来实现 iOS11 上的大标题
+//    return CustomScrollView(
+//      slivers: const <Widget>[
+//        CupertinoSliverNavigationBar(
+//          largeTitle: Text('Search'),
+//        ),
+//      ],
+//    );
   }
 }
